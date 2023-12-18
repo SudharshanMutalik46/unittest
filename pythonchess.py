e928b749d6466a90e958019b70cd01974903b32e
Sudharshan Mutalik <114343463+SudharshanMutalik46@users.noreply.github.com>
2023-12-19 00:45:00 +0530

    Update pythonchess.py

--- pythonchess.py
+++ pythonchess.py
@@ -1,35 +1,28 @@
-"""
-Example written for Qxf2 Services' blog post on Python Unit Checking
-Check if IsCheckmate method in ChessRules class calls GetListOfValidMoves  
-Assert that the mocked method is called 16 times
-"""
-import unittest, mock
-import ChessRules 
-from ChessBoard import ChessBoard
- 
-class CheckIsCheckmate(unittest.TestCase):
-    "Class to unit check the IsCheckmate method of ChessRules.py module"
-    # creating a mock for GetListOfValidMoves
-    @mock.patch('ChessRules.ChessRules.GetListOfValidMoves')        
-    def test_call_count(self, mockGetListOfValidMoves):
-        "Unit test for ChessRules method: IsCheckmate"
- 
-        # Creating objects of Chessboard and ChessRules class and calling IsCheckmate function with each piece for initial position and "black" color
-        cb = ChessBoard(0) #Create a chess board object with the initial position
-        chess_rules_obj = ChessRules.ChessRules()
-        chess_rules_obj.IsCheckmate(cb.GetState(),"black")
- 
-        # Create expected_arg_calls list which is supposed to be called with GetListOfValidMoves for initial position
-        # IsCheckmate calls GetListOfValidMoves with arguments: board, color (who is to play?) and co-ordinates of a square with a piece on it
-        expected_arg_calls = []
-        for row in range(0,2):
-            for col in range(0,8):
-                expected_arg_calls.append(mock.call(cb.GetState(), 'black', (row, col)))
- 
- 
-        # check the number of times your mocked method was called
-        self.assertEqual(mockGetListOfValidMoves.call_count,16)
- 
- 
-if __name__=="__main__":
+import unittest
+import requests
+
+class TestATGWorldWebsite(unittest.TestCase):
+    
+    def test_website_availability(self):
+        """
+        Test that the ATG World website is available and loads properly.
+        """
+        url = 'https://atg.world'
+        
+        try:
+            response = requests.get(url)
+            # Print statements to log activity
+            print(f"Attempting to connect to {url}")
+            
+            # The website is considered up if the status code is 200
+            self.assertEqual(response.status_code, 200)
+            print("Connection successful. Website is up.")
+            
+        except requests.RequestException as e:
+            # If any exception occurs during the request, the test fails
+            self.fail(f"Website could not be reached. Error: {e}")
+            print("Connection failed. Website is down.")
+
+# This allows the test to be run from the command line
+if __name__ == '__main__':
     unittest.main()
