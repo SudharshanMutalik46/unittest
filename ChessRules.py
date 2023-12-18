aaa2d0826bfbc6c48081e9691222104c6de9e69e
Sudharshan Mutalik <114343463+SudharshanMutalik46@users.noreply.github.com>
2023-12-19 00:54:12 +0530

    Update ChessRules.py

--- ChessRules.py
+++ ChessRules.py
@@ -1,56 +1,44 @@
-class ChessRules:
-    def __init__(self):
-        # Initialize the chessboard or any other necessary data structures
-        # You can implement the board as a list, dictionary, or a custom class
-
-    def is_checkmate(self, board, color):
-        """
-        Check if the given player's king is in checkmate.
-        
-        Parameters:
-        - board: The current state of the chessboard.
-        - color: The color of the player to check for checkmate ('white' or 'black').
-        
-        Returns:
-        - True if the player is in checkmate, False otherwise.
-        """
-        # Implement your checkmate logic here
-        # This can involve checking for moves, checking for threats, etc.
-
-        # For the sake of example, assume checkmate if the king is under threat
-        king_position = self.get_king_position(board, color)
-        if self.is_square_under_threat(board, king_position, color):
-            return True
-        else:
-            return False
-
-    def get_king_position(self, board, color):
-        """
-        Get the position of the king for the given player.
-        
-        Parameters:
-        - board: The current state of the chessboard.
-        - color: The color of the player to get the king position ('white' or 'black').
-        
-        Returns:
-        - The position of the king as a tuple (row, column).
-        """
-        # Implement logic to find and return the king's position
-        # This could involve iterating through the board and checking piece types
-
-    def is_square_under_threat(self, board, square, color):
-        """
-        Check if the given square is under threat from the opponent.
-        
-        Parameters:
-        - board: The current state of the chessboard.
-        - square: The position to check as a tuple (row, column).
-        - color: The color of the player whose king is being checked.
-        
-        Returns:
-        - True if the square is under threat, False otherwise.
-        """
-        # Implement logic to check if the square is under threat
-        # This could involve checking for attacks from opponent pieces
-
-# Additional methods and logic can be added based on the requirements of your chess game.
+"""
+Example written for Qxf2 Services' blog post on Python Unit Checking
+Check if IsCheckmate method in ChessRules class calls GetListOfValidMoves  
+Assert that it called with the expected arguments
+"""
+import unittest, mock
+import ChessRules 
+from ChessBoard import ChessBoard
+ 
+class CheckIsCheckmate(unittest.TestCase):
+    "Class to unit check the IsCheckmate method of ChessRules.py module"
+    # creating a mock for GetListOfValidMoves
+    @mock.patch('ChessRules.ChessRules.GetListOfValidMoves')        
+    def test_Checkmate(self, mockGetListOfValidMoves):
+        "Unit test for ChessRules method: IsCheckmate"
+        # NOTE 1: We only check that there are valid moves for a given color
+        # NOTE 2: We are NOT checking the setting color logic in this unit test. You need to write another unit test for this logic.
+ 
+        # Creating objects of Chessboard and ChessRules class and calling IsCheckmate function with each piece for initial position and "black" color
+        cb = ChessBoard(0) #Create a chess board object with the initial position
+        chess_rules_obj = ChessRules.ChessRules()
+        chess_rules_obj.IsCheckmate(cb.GetState(),"black")
+ 
+        # Create expected_arg_calls list which is supposed to be called with GetListOfValidMoves for initial position
+        # IsCheckmate calls GetListOfValidMoves with arguments: board, color (who is to play?) and co-ordinates of a square with a piece on it
+        expected_arg_calls = []
+        for row in range(0,2):
+            for col in range(0,8):
+                expected_arg_calls.append(mock.call(cb.GetState(), 'black', (row, col)))
+ 
+        # Assert that the mockGetListOfValidMoves.call_args_list matches expected_arg_calls
+        self.assertEqual(mockGetListOfValidMoves.call_args_list, expected_arg_calls)
+ 
+        # DID YOU KNOW?
+        # assert_any_call can be used to assert a method that was called at least once with some arguments        
+        #mockGetListOfValidMoves.assert_any_call(cb.GetState(),"black",(1,6))
+ 
+        # DID YOU KNOW?
+        # call_count can be used to check the number of times your mocked method was called
+        #self.assertEqual(mockGetListOfValidMoves.call_count,16)
+ 
+ 
+if __name__=="__main__":
+    unittest.main()
